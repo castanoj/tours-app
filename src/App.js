80055263c48a96fdc1d7ece4f95ed6ee34c952e9
castanoj <jorgedcastano98@gmail.com>
2022-07-13 20:05:50 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './components/Loading';
 import Tours from './components/Tours';
-
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project';
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
@@ -14,7 +16,6 @@ function App() {
 
   const fetchTours = async () => {
     setLoading(true);
-
     try {
       const response = await fetch(url);
       const tours = await response.json();
@@ -22,15 +23,12 @@ function App() {
       setTours(tours);
     } catch (error) {
       setLoading(false);
+      console.log(error);
     }
-
-    console.log(tours);
   };
-
   useEffect(() => {
     fetchTours();
   }, []);
-
   if (loading) {
     return (
       <main>
@@ -38,20 +36,18 @@ function App() {
       </main>
     );
   }
-
   if (tours.length === 0) {
     return (
       <main>
         <div className="title">
           <h2>no tours left</h2>
-          <button className="btn" onClick={fetchTours}>
+          <button className="btn" onClick={() => fetchTours()}>
             refresh
           </button>
         </div>
       </main>
     );
   }
-
   return (
     <main>
       <Tours tours={tours} removeTour={removeTour} />
